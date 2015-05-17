029925bfffb961a4300ca0c23462228768dead05
scatna <scatna@gmail.com>
2015-05-17 13:09:13 -0500

    ...

--- src/main/java/test/testing/App.java
+++ src/main/java/test/testing/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 1234543678910 1234567890!" );
+        System.out.println( "Hello World 1234567890!" );
     }
 }
